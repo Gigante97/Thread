e90930eb428c619f99ad530ee3c27cb4e548b9c6
Kirill_Gigante <kdgigante@yandex.ru>
2021-03-01 13:43:20 +0300

    Better version of homework 5 homework completed

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -22,17 +23,13 @@ public class Main {
 
     private static float[] createArray () {
         float [] array = new float[size];
-        for (int i = 0;i< array.length; i++){
-            array[i]=1;
-        }
+        Arrays.fill(array, 1);//вместо цикла for
         return array;
     }
 
     private static void oneThread (float[] array) {
         long startTime = System.currentTimeMillis();
-        for (int i =0; i < array.length; i++) {
-            array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
+        modificateArray(array);
         System.out.println("Время выполнения программы в 1 поток(в милисекундах):");
         System.out.println(System.currentTimeMillis()- startTime);
     }
@@ -43,21 +40,11 @@ public class Main {
         long start = System.currentTimeMillis();
         System.arraycopy(array, 0, firstArray, 0, h);
         System.arraycopy(array, h, secondArray, 0, h);
-        Thread calculateFirst = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i =0; i<firstArray.length; i++) {
-                    firstArray[i] = (float)(firstArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
-            }
+        Thread calculateFirst = new Thread(() -> { //IDEA считает, что лямбда выражение лучше
+          modificateArray(firstArray); //IDEA не нравился дублирующийся код
         });
-        Thread calculateSecond = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i =0; i<secondArray.length; i++) {
-                    secondArray[i] = (float)(secondArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
-            }
+        Thread calculateSecond = new Thread(() -> {
+            modificateArray(secondArray);
         });
         calculateFirst.start();
         calculateSecond.start();
@@ -73,5 +60,12 @@ public class Main {
         System.arraycopy(secondArray,0,array,h,h);
         System.out.println("Время выполнения программы в 2 потока(в милисекундах):");
         System.out.println(System.currentTimeMillis()-start);
+    }
+
+    private static void modificateArray(float[] array) {
+        for (int i =0; i<array.length; i++) {
+            array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
     }
 }
